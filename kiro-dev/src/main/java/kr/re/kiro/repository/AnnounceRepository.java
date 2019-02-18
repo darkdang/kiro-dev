d03e2da48b8421200bc32a8b1aa6bf11603003e8
darkdang <darkdang@torpedo.co.kr>
2019-02-18 21:09:00 +0900

    modify by announce repository

---
+++ kiro-dev/src/main/java/kr/re/kiro/repository/AnnounceRepository.java
@@ -0,0 +1,21 @@
+package kr.re.kiro.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import kr.re.kiro.model.Announce;
+
+/**
+ * <pre>
+ * kr.re.kiro.repository
+ * AnnounceRepository.java
+ * </pre>
+ *
+ * @Author : Kim sungkeun
+ * @Date   : 2019. 2. 18.
+ * @Version: 
+ */
+@Repository
+public interface AnnounceRepository extends JpaRepository<Announce, Integer> {
+
+}

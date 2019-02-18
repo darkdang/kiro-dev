33e45bf01dc7106c108213701240991569dfa4f5
darkdang <darkdang@torpedo.co.kr>
2019-02-18 21:08:41 +0900

    modify by user repository

--- kiro-dev/src/main/java/kr/re/kiro/repository/UserRepository.java
+++ kiro-dev/src/main/java/kr/re/kiro/repository/UserRepository.java
@@ -1,5 +1,24 @@
 package kr.re.kiro.repository;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import kr.re.kiro.model.User;
+
+/**
+ * <pre>
+ * kr.re.kiro.repository
+ * UserRepository.java
+ * </pre>
+ *
+ * @Author : Kim sungkeun
+ * @Date   : 2019. 2. 18.
+ * @Version: 
+ */
+@Repository
+public interface UserRepository  extends JpaRepository<User, Integer> {
+
+	@Query
+	public User findByLoginId(String loginId);
 }
